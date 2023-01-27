df9f91377545bcc3128e1342bb09cf814706df4b
Mordremoth <wantedlordd@gmail.com>
2023-01-27 20:09:56 +0300

    initialized project with next.js tailwind and thirdweb

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
+const activeChainId = ChainId.Mumbai;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
